2867067956972d9ff4a62dc8004d151d4dc94f89
Ayush Gupta <ayush2362000@gmail.com>
2024-02-23 19:17:17 +0530

    Seperated APIs

---
+++ React/budget-master-v1/src/Navigation/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+
+const Home = () => {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <Sidebar />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
+ 
